8929c4a1c492bc324eede0135f36792672a0c025
Akshaypareek01 <akshay96102@gmail.com>
2024-07-09 11:15:41 +0530

    working

--- src/app/Config/BaseUrl.jsx
+++ src/app/Config/BaseUrl.jsx
@@ -3,4 +3,4 @@
 export const BASE_URL="https://chargesol-backend.onrender.com/api"
 // export const Base_url= "https://apis.samsarawellness.in/"
 // export const Base_url= "http://localhost:4000/"
-export const Base_url= "https://samsara-backend.onrender.com"
+export const Base_url= "https://samsara-backend.onrender.com/"
